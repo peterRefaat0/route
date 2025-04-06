adf087b09d059b1052287ff80aee20a37e13099f
peterRefaat0 <peterrefaat346@gmail.com>
2025-04-06 23:54:18 +0200

    Remove hardcoded URL from checkout session API call in CartService

--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -38,7 +38,7 @@ export class CartService {
   checkOut(cartId: string | null, orderInfo: object): Observable<any> {
     return this._HttpClient.post(
       this.baseUrl +
-        `orders/checkout-session/${cartId}?url=https://github.com/ahmed-menisy/fresh-live-route`,
+        `orders/checkout-session/${cartId}`,
 
       {
         shippingAddress: orderInfo,
